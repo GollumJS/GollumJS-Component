3c7b84580dd7fb24a69638dd88bf117d405f28b9
SmeagolWorms4 <smeagolworms4@gmail.com>
2016-04-01 00:06:12 +0200

    Preloading image

--- build/gollumjs-component-min.js
+++ build/gollumjs-component-min.js
@@ -633,25 +633,47 @@ GollumJS.NS(GollumJS.Component, function() {
 			var preloadEl = $('componentprelaod');
 			
 			var jsPromiseLoading = Promise.resolve();
-
+			
+			var loadedDiv = $('<div style="position: fixed; top: -30000px; left: -30000px;" ></div>').appendTo(document.body);
+			
 			return Collection.eachStep (preloadEl, function (i, value, step) {
 				
 				var el = $(value);
 				var id = el.attr('id');
-				var component = _this.manager.getComponent(id)
-					.setJsPromiseLoading(jsPromiseLoading)
-				;
 				
-				jsPromiseLoading = component.load()
-					.then(function () {
-						console.log ('Preloading component:', id);
+				if (id) {
+					
+					var component = _this.manager.getComponent(id)
+						.setJsPromiseLoading(jsPromiseLoading)
+					;
+					
+					jsPromiseLoading = component.load()
+						.then(function () {
+							console.log ('Preloading component:', id);
+							step();
+						})
+						.catch(console.error)
+					;
+				}
+				
+				var img = el.attr('img');
+				if (img) {
+					var image = new Image();
+					image.onload = function() {
+						console.log ('Preloading image:', img);
 						step();
-					})
-					.catch(console.error)
-				;
+					};
+					image.src = img;
+					loadedDiv.append(image);
+				}
 				
-			});
+			})
+				.then(function () {
+					loadedDiv.remove();
+				})
+			;
 		}
+		
 
 	});
 
